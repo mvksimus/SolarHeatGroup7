2351d345eb8da110465e096e6abd3f3fd464dd0e
Nikolas Giemetzoglou <n.giemetzoglou@student.tue.nl>
2025-09-13 17:31:28 +0200

    Code looks good, but produces some imaginary solutions. Fix needed

--- IntexSolarMat1.m
+++ IntexSolarMat1.m
@@ -1,10 +1,13 @@
-T_wi = 15;
+close; clear all; clc;
+
+T_wi = 15+273;
 T = T_wi; % initial temperature in storage
 T_env = 293; % Ambient temperature of air
 cd_water = 4.18; % Approximate specific heat of water (It changes slighlty with temperature)
 irradiance = 1000;
 density = 1000; % Approximately
 S_B = 5.67*10e-9; % Stefan-Boltzmann
+h_c_air = 25.5; % Natural convection, 25-20 C, 1 atm
 
 % tube (storage to pump)
 
@@ -91,18 +94,18 @@ S_B = 5.67*10e-9; % Stefan-Boltzmann
 
 % storage
 
-h_air = 25.5; % Natural convection, 25-20 C, 1 atm
+
 
 k_pvc = 0.19;
 k_i = 0.022;
 k_floor = 0.30;
-k_al = ;
+k_al = 237;
 k_PU = 0.13;
 
-em_pvc = ;
-em_i = ;
-em_wood = ;
-em_PU = ;
+em_pvc = 0.92;
+em_i = 0.8;     % Approximate values based on color
+em_floor = 0.1; % and intuition. Exact values
+em_PU = 0.9;    % could not be found on the Internet.
 
 d_in = 0.0085;
 d_out = 0.012;
@@ -117,50 +120,57 @@ R_rad_tube = 1/(S_B*2*pi*em_PU*(d_out/2)*L_c_to_s);
 volume_tubes = pi*L_s_to_c*(d_in/2)^2;
 volume_tube = pi*L_c_to_s*(d_in/2)^2;
 
-R_cond_hose = ;
-R_conv_hose = ;
-R_rad_hose = ;
-volume_hose = ;
+%R_cond_hose = ;
+%R_conv_hose = ;
+%R_rad_hose = ;
+%volume_hose = ;
 
-R_cond_collector = ;
-R_conv_collector = ;
-R_rad_collector = ;
-volume_collector = ;
+d_c = 0.001;     % Not mentioned
+d_al = 0.002;    %
+d_table = 0.006;
+A_c = 1.44;
+R_cond_collector = 1/(1/(d_c/(A_c*k_pvc))+1/((d_c/(A_c*k_pvc))+(d_al/(A_c*k_al))+(d_table/(A_c*k_floor))));
+R_conv_collector = 1/(2*h_c_air*A_c);
+R_rad_collector = 1/(S_B*A_c*(em_floor+em_pvc));
+volume_collector = 0.005; % Max volume is about 10 liters. Assuming half full to not waste water.
 
 D_t = 0.11;
-d_wall = 0.002;
+d_wall = 0.002; % Not mentioned
 d_i = 0.02;
-d_lid = 0.005;
-d_floor = 0.01;
+d_lid = 0.005;  %
+d_floor = 0.01; %
+d_tube = d_out;
 H = 0.2;
 A_floor = pi*(D_t/2)^2;
 A_lid = A_floor - 2*pi*(d_tube/2)^2;
-R_cond_storage = 1/(1/(log((D_t/2+d_wall)/(D_t/2))/(2*pi*k_pvc*H)+log((D_t/2+d_wall+d_i)/(D_t/2+d_wall))/(2*pi*k_i*H))+1/((d_wall/(A_floor*k_pvc))+(d_i/(A_floor*k_i))+(d_wood/(A_floor*k_floor)))+1/(d_lid/(A_lid*k*pvc)));
-R_conv_storage = 1/(h_c_air*2*pi*((D_t/2+d_wall+d_i)/2)*H)+1/(h_c_air*2*pi*A_floor)+1/(h_c_air*2*pi*A_lid);
-R_rad_storage = 1/(S_B*2*pi*em_i*((D_t/2+d_wall+d_i)/2)*H)+1/(S_B*2*pi*em_wood*A_floor)+1/(S_B*2*pi*em_pvc*A_lid);
-volume_storage = 2*10^-3;
+R_cond_storage = 1/(1/(log((D_t/2+d_wall)/(D_t/2))/(2*pi*k_pvc*H)+log((D_t/2+d_wall+d_i)/(D_t/2+d_wall))/(2*pi*k_i*H))+1/((d_wall/(A_floor*k_pvc))+(d_i/(A_floor*k_i))+(d_floor/(A_floor*k_floor)))+1/(d_lid/(A_lid*k_pvc)));
+R_conv_storage = 1/((h_c_air*2*pi*((D_t/2+d_wall+d_i)/2)*H)+(h_c_air*2*pi*A_floor)+(h_c_air*2*pi*A_lid));
+R_rad_storage = 1/((S_B*2*pi*em_i*((D_t/2+d_wall+d_i)/2)*H)+(S_B*2*pi*em_floor*A_floor)+(S_B*2*pi*em_pvc*A_lid));
+volume_storage = 0.002;
 
 for section = 1:10
 % tube (storage to pump)
-T1 = HeatLossPerSection (T, T_env, R_cond_tube, R_conv_tube, R_rad_tube, mass_tubes, cd_water, irradiance, 0);
+T1 = HeatLossPerSection (T, T_env, R_cond_tubes, R_conv_tubes, R_rad_tubes, density, volume_tubes, cd_water, irradiance, 0);
 % pump segment?
+% I think not necessairy.
+%T2 = HeatLossPerSection (T1, T_env, R_cond_pump, R_conv_pump, R_rad_pump, density, volume_pump, cd_water, irradiance, 0);
 % tube (from pump to hose)
-T2 = HeatLossPerSection (T1, T_env, R_cond_tube, R_conv_tube, R_rad_tube, density, volume_tubes, cd_water, irradiance, 0);
+T2 = HeatLossPerSection (T1, T_env, R_cond_tubes, R_conv_tubes, R_rad_tubes, density, volume_tubes, cd_water, irradiance, 0);
 % hose (tube connection to collector)
-T3 = HeatLossPerSection (T2, T_env, R_cond_hose, R_conv_hose, R_rad_hose, density, volume_hose, cd_water, irradiance, sunlit_area_h);
+%T3 = HeatLossPerSection (T2, T_env, R_cond_hose, R_conv_hose, R_rad_hose, density, volume_hose, cd_water, irradiance, sunlit_area_h);
 % collector
-T4 = HeatLossPerSection (T3, T_env, R_cond_collector, R_conv_collector, R_rad_collector, density, volume_collector, cd_water, irradiance, sunlit_area_c);
+T3 = HeatLossPerSection (T2, T_env, R_cond_collector, R_conv_collector, R_rad_collector, density, volume_collector, cd_water, irradiance, sunlit_area_c);
 % hose (collector to tube connection)
-T5 = HeatLossPerSection (T4, T_env, R_cond_hose, R_conv_hose, R_rad_hose, density, volume_hose, cd_water, irradiance, sunlit_area_h);
+%T5 = HeatLossPerSection (T4, T_env, R_cond_hose, R_conv_hose, R_rad_hose, density, volume_hose, cd_water, irradiance, sunlit_area_h);
 % tube (hose to storage)
-T6 = HeatLossPerSection (T5, T_env, R_cond_tube, R_conv_tube, R_rad_tube, density, volume_tube, cd_water, irradiance, 0);
+T4 = HeatLossPerSection (T3, T_env, R_cond_tube, R_conv_tube, R_rad_tube, density, volume_tube, cd_water, irradiance, 0);
 % storage
-T = HeatLossPerSection (T6, T_env, R_cond_storage, R_conv_storage, R_rad_storage, density, volume_storage, cd_water, irradiance, 0);
+T = HeatLossPerSection (T4, T_env, R_cond_storage, R_conv_storage, R_rad_storage, density, volume_storage, cd_water, irradiance, 0);
 end
 
 function [T] = HeatLossPerSection (T, T_env, R_cond, R_conv, R_rad, density, volume_segment, cd_water, irradiance, sunlit_area)
 
-    %
+    % Documentaion
     %
     %
     %
@@ -169,11 +179,30 @@ function [T] = HeatLossPerSection (T, T_env, R_cond, R_conv, R_rad, density, vol
 
     T_w = T;
     
-    energy_balance = irradiance*sunlit_area - (1/R_cond) * (T_outer - T_w) - ... % conduction from water to outer part of segment
-                (1/R_conv) * (T_outer - T_env) - ... % convection to air
-                (1/R_rad) * (T_outer^4 - T_env^4) == 0; % radiation to surroundings
+    % energy_balance = irradiance*sunlit_area - (1/R_cond) * (T_outer - T_w) - ... % conduction from water to outer part of segment
+    %             (1/R_conv) * (T_outer - T_env) - ... % convection to air
+    %             (1/R_rad) * (T_outer^4 - T_env^4) == 0; % radiation to surroundings
 
-    T_outer = solve(energy_balance, T_outer); % solve for the temperature at the outer part of the tube
+    % (1/R_rad) * (T_outer^4) + (1/R_cond + 1/R_conv) * T_outer - (irradiance*sunlit_area + (1/R_rad) * (T_env^4) + (1/R_cond + 1/R_conv) * T_w) = 0; 
+
+    coef = [(1/R_rad), 0, 0, (1/R_cond + 1/R_conv), -((irradiance*sunlit_area)+(T_env^4/R_rad)+(1/R_cond+1/R_conv)*T_w)];
+    T_outer_solutions = roots(coef);
+    disp(T_outer_solutions)
+    
+    T_outer = 273;
+
+    for solution = 1:4
+        if isreal(T_outer_solutions(solution)) && (abs(T_outer_solutions(solution)-273)) >= (abs(T_outer-273))
+            T_outer = T_outer_solutions(solution);
+        end
+    end
+    
+    if T_outer == 273
+        error("No real solutions")
+    end
+    disp(T_outer)
+
+    % T_outer = solve(energy_balance, T_outer); % solve for the temperature at the outer part of the tube
 
     Q_cond = (1/R_cond) * (T_w - T_outer); % solve for heat conducted from water to outer surface
 
